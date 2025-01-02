557ce0c8ff416bdd27059eef425c825f338b042f
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2025-01-02 20:49:59 +0600

    project section updated

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Portfolio from "./assets/Portfolio.png"
-import SpicyBites from "./assets/SpicyBites.png"
-import Youtube from "./assets/Youtube.png"
-import Webelite from "./assets/Webelite.png"
-import Supercar from "./assets/Supercar.png"
+// import Portfolio from "./assets/Portfolio.png"
+import Travel from "./assets/travel.png"
+// import SpicyBites from "./assets/SpicyBites.png"
+import YTNike from "./assets/Yt-Nike.png"
+// import Youtube from "./assets/Youtube.png"
+import YTCloth from "./assets/YT-Cloth.png"
+// import Webelite from "./assets/Webelite.png"
+import Rescue from "./assets/rescue.png"
+// import Supercar from "./assets/Supercar.png"
+import Dragon from "./assets/dragon.png"
 
 import Cards from './components/Cards'
 
@@ -12,39 +16,39 @@ const Projects = () => {
 
     const projectJson = [
         {
-          title: 'Portfolio',
-          desc: 'Webelite Builders did an amazing job on our website. Their professionalism and dedication to our project were outstanding.',
-          image: Portfolio,
-        //   live: "https://rohitsingh93300-portfolio.vercel.app/",
-        //   github: "https://github.com/rohitsingh93300/portfolio"
+          title: 'YT-Cloth',
+          desc: 'YT-Cloth is a responsive React.js project styled with Tailwind CSS, featuring fashion blogs and videos for men, women, and kids. It includes dynamic routing, an interactive UI, and fast performance. Deployed on Vercel, it provides a seamless and modern user experience across all devices.',
+          image: YTCloth,
+          live: "https://yt-cloth.vercel.app/",
+          github: "https://github.com/yeasinshaikh188/YT-Cloth"
         },
         {
-          title: 'Spicy Bites',
-          desc: 'The team at Webelite Builders exceeded our expectations with their digital marketing expertise. Highly recommend!',
-          image: SpicyBites,
-        //   live: "https://spicybites.netlify.app/",
-        //   github: "https://github.com/rohitsingh93300/YtSpicyBites"
+          title: 'Travel',
+          desc: 'Travel Explorer is a responsive single-page React.js application styled with Tailwind CSS. It features a sleek design, smooth navigation, and showcases travel destinations. Fully optimized for all devices and deployed on Vercel, it ensures fast performance and a seamless user experience, making it perfect for exploring travel inspirations effortlessly.',
+          image: Travel,
+          live: "https://trip-buddy-ol98.vercel.app/",
+          github: "https://github.com/yeasinshaikh188/TripBuddy"
         },
         {
-          title: 'YouTube Clone',
-          desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-          image: Youtube,
-        //   live: "https://youtube-clone-93300.netlify.app/",
-        //   github: "https://github.com/rohitsingh93300/YouTube-clone"
+          title: 'Dragon',
+          desc: 'Dragon News is a React app with Firebase authentication, dynamic news from data, styled using Tailwind CSS and DaisyUI.',
+          image: Dragon,
+          live: "https://react-dragon-news-auth-b9f84.web.app/",
+          github: "https://github.com/yeasinshaikh188/react-dragon-news-auth"
         },
         {
-          title: 'Webelite builder',
-          desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-          image: Webelite,
-        //   live: "https://www.webelitebuilders.com/",
-        //   github: "#"
+          title: 'YT-Nike',
+          desc: 'YT-Nike is a responsive React application styled with Tailwind CSS and enhanced with Framer Motion for smooth animations. It features dedicated blogs showcasing boys, women s, and kids footwear collections. The sleek and modern design ensures a user-friendly experience on all devices, making it an engaging platform for exploring trendy shoe styles and fashion insights.',
+          image: YTNike,
+          live: "https://yt-nike.vercel.app/",
+          github: "https://github.com/yeasinshaikh188/YT-Nike"
         },
         {
-          title: 'Super Car',
-          desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-          image: Supercar,
-        //   live: "https://supercar123.netlify.app/",
-        //   github: "https://github.com/rohitsingh93300/supercars"
+          title: 'Rescue',
+          desc: 'Rescue is a responsive React application styled with Tailwind CSS and deployed on Vercel. It features routing for seamless navigation and integrates a map displayed through an interactive image. Designed with a clean and modern layout, Rescue ensures a smooth user experience across all devices, making it ideal for exploring location-based services effortlessly.',
+          image: Rescue,
+          live: "https://rescue-react-project.vercel.app/",
+          github: "https://github.com/yeasinshaikh188/rescue-react-project"
         },
       ];
 
